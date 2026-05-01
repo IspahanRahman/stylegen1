02212d5dad292af69693d1c16ea12d969b5e55c8
IspahanRahman <ispahanrahman133@gmail.com>
2026-05-01 20:01:00 +0600

    feat: integrate Tailwind CSS and create authentication pages
    - Added Tailwind CSS and its plugins for forms and typography in package.json.
    - Updated globals.css to include Tailwind directives and custom styles.
    - Created postcss.config.js for Tailwind CSS configuration.
    - Implemented Login and Register pages in the authentication section.
    - Developed a Login component with basic structure and styling.
    - Configured tailwind.config.js with custom colors and font families.

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Register () {
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <p>This is the register page of our Next.js application.</p>
+    </div>
+  )
+}
---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Login () {
+  return (
+    <div>
+      <h1 className="text-red-900 bg-orange-500">Login Page</h1>
+      <p>This is the login page of our Next.js application.</p>
+    </div>
+  )
+}
